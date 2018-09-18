079a4431ed6a90d20bd090ecddebd02bc7870f4a
Mustafa <Mustafastation@gmail.com>
2018-09-17 23:38:32 -0700

    added download_response to core functionality
    downloads url .mat file and parses into variable

--- SiEPIC_Photonics_Package/core.py
+++ SiEPIC_Photonics_Package/core.py
@@ -8,19 +8,21 @@ Module:     Core functionalities of SiEPIC PP
 
 functions:
 
-calibrate( input_response, reference_response):
-baseline_correction( input_response ):
+calibrate( input_response, reference_response): response correction function to calibrate an input response with respect to a reference response
+baseline_correction( input_response ): baseline correction function to flatten a response with respect to it self
 cutback( input_data_response, input_data_count):
 to_s_params( input_data ):
+download_response (url, port): downloads input .mat response from a url and parses it into array
 """
 
 from SiEPIC_Photonics_Package.setup import *
 
+#%% calibration function
 def calibrate( input_response, reference_response):
     print("called")
     return
 
-# baseline correction function
+#%% baseline_correction function
 # input list format: input_response[wavelength (nm), power (dBm)]
 # output list format: [power (dBm) with baseline correction, baseline correction fit]
 def baseline_correction( input_response ):
@@ -36,9 +38,27 @@ def baseline_correction( input_response ):
     
     return [power_corrected, power_baseline]
 
+#%% cutback function
 def cutback( input_data_response, input_data_count):
     return
 
+#%% to_s_params function
 def to_s_params( input_data ):
     return
 
+#%% download_response function
+# download input .mat url data
+# outputs parsed data array [wavelength (m), power (dBm)]
+# data is assumed to be from automated measurement scanResult format
+def download_response ( url, port):
+    r = requests.get(url,allow_redirects=True)
+    file_name = 'downloaded_data'+str(port)
+    with open(file_name, 'wb') as f:
+        f.write(r.content)
+        
+    data = scipy.io.loadmat(file_name)
+
+    wavelength = data['scanResults'][0][port][0][:,0]
+    power = data['scanResults'][0][port][0][:,1]
+    data = [wavelength,power]
+    return data
--- workspace.py
+++ workspace.py
@@ -4,7 +4,7 @@ SiEPIC Photonics Package
 Author:     Mustafa Hammood
             Mustafa@ece.ubc.ca
 
-Example:    Application of SiEPIC_PP baseline correction function
+Example:    Application of SiEPIC_PP calibration function
 """
 
 #%% import package and installed dependent packages
@@ -16,46 +16,34 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath((os.path.dirname
 import SiEPIC_Photonics_Package as SiEPIC_PP
 from SiEPIC_Photonics_Package.setup import *
 
+#%% calibration function
+# input list format: x_response[wavelength (nm), power (dBm)]
+# output list format: calibrated_response (dB), insertion_losses (dB)
+def calibrate( input_response, reference_response):
+    print("called")
+    return
+
 #%% download .mat file from GitHub repo and parse it to a variable (data)
+# response to be calibrated
 file_name = 'MZI_data'
 file_extension = '.mat'
 url = 'https://github.com/mustafacc/SiEPIC_Photonics_Package/blob/master/Examples/'+file_name+file_extension+'?raw=true'
-r = requests.get(url,allow_redirects=True)
-with open(file_name+file_extension, 'wb') as f:
-    f.write(r.content)
-    
-data = scipy.io.loadmat(file_name+file_extension)
 PORT = 0
+input_response= SiEPIC_PP.core.download_response(url,PORT)
 
-wavelength = data['scanResults'][0][PORT][0][:,0]
-power = data['scanResults'][0][PORT][0][:,1]
+#%% apply SiEPIC_PP calibration correction function
 
-#%% apply SiEPIC_PP baseline correction function
-input_response= [wavelength,power]
-[power_corrected, pfit] = SiEPIC_PP.core.baseline_correction( input_response )
+[power_corrected, pfit] = calibrate( input_response )
 
 #%% plot response and save pdf
 # raw response with baseline fit
 matplotlib.pyplot.figure(0)
 fig1 = matplotlib.pyplot.plot(wavelength,power, label='Raw data', color='red')
-fig2 = matplotlib.pyplot.plot(wavelength,pfit, label = 'Data baseline', color='blue')
 matplotlib.pyplot.legend(loc=0)
 matplotlib.pyplot.ylabel('Power (dBm)', color = 'black')
 matplotlib.pyplot.xlabel('Wavelength (nm)', color = 'black')
 matplotlib.pyplot.setp(fig1, 'linewidth', 2.0)
-matplotlib.pyplot.setp(fig2, 'linewidth', 2.0)
 matplotlib.pyplot.xlim(round(min(wavelength)),round(max(wavelength)))
 matplotlib.pyplot.title("Experimental data (raw)")
 matplotlib.pyplot.savefig(file_name+'.pdf')
-matplotlib.rcParams.update({'font.size': 14, 'font.family' : 'Times New Roman', 'font.weight': 'bold'})
-
-# corrected response
-matplotlib.pyplot.figure(1)
-fig = matplotlib.pyplot.plot(wavelength,power_corrected)
-matplotlib.pyplot.ylabel('Power (dBm)', color = 'black')
-matplotlib.pyplot.xlabel('Wavelength (nm)', color = 'black')
-matplotlib.pyplot.setp(fig, 'color', 'r', 'linewidth', 2.0)
-matplotlib.pyplot.xlim(round(min(wavelength)),round(max(wavelength)))
-matplotlib.pyplot.title("Experimental data (baseline corrected)")
-matplotlib.pyplot.savefig(file_name+'_baseline'+'.pdf')
 matplotlib.rcParams.update({'font.size': 14, 'font.family' : 'Times New Roman', 'font.weight': 'bold'})
