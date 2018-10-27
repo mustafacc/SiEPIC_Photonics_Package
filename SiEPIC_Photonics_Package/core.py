be850ef2f5cfb5d7f2b72c16c689e6e28759e42b
Mustafa Hammood <mustafa@ece.ubc.ca>
2018-10-27 14:14:54 -0700

    core: Added calibration function

--- SiEPIC_Photonics_Package/core.py
+++ SiEPIC_Photonics_Package/core.py
@@ -18,13 +18,24 @@ download_response (url, port): downloads input .mat response from a url and pars
 from SiEPIC_Photonics_Package.setup import *
 
 #%% calibration function
+# input list format: [input_response[wavelength (nm), power (dBm)], reference_response[wavelength (nm), power (dBm)]]
+# output list format: [input power (dBm) with calibration correction, polynomial fit of reference power (dBm)]
 def calibrate( input_response, reference_response):
-    print("called")
-    return
+    # fit the calibration response to a polynomial
+    fitOrder = 8
+    wavelength = reference_response[0]
+    power = reference_response[1]
+    
+    pfit = numpy.polyfit(wavelength-numpy.mean(wavelength), power, fitOrder)
+    power_calib_fit = numpy.polyval(pfit, wavelength-numpy.mean(wavelength))
+    
+    power_corrected = input_response[1] - power_calib_fit
+    
+    return [power_corrected, power_calib_fit]
 
 #%% baseline_correction function
 # input list format: input_response[wavelength (nm), power (dBm)]
-# output list format: [power (dBm) with baseline correction, baseline correction fit]
+# output list format: [input power (dBm) with baseline correction, baseline correction fit]
 def baseline_correction( input_response ):
     fitOrder = 4
     wavelength = input_response[0]
