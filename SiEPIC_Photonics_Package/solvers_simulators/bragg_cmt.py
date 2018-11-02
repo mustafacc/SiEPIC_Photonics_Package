3a097076bf8e9b2d67175232b204985796f561fb
Mustafa <Mustafastation@gmail.com>
2018-11-01 20:12:17 -0700

    solvers: Bragg CMT

---
+++ SiEPIC_Photonics_Package/solvers_simulators/bragg_cmt.py
@@ -0,0 +1,106 @@
+"""
+SiEPIC Photonics Package 
+
+Author:     Mustafa Hammood
+            Mustafa@ece.ubc.ca
+
+            https://github.com/SiEPIC-Kits/SiEPIC_Photonics_Package
+            
+Solvers and simulators: Bragg simulator using coupled-mode theory (CMT) approach
+reference: "Bragg grating based Fabry-Perot filters for characterizing silicon-on-insulator waveguides",
+Y. Painchaud, Group IV Photonics, 2012
+
+Based on A. Mistry MATLAB implementation
+"""
+#%% dependent packages
+import numpy as np
+import math, cmath, matplotlib
+from numpy.lib.scimath import sqrt as csqrt
+
+#%% user input
+wavelength_start = 1500e-9
+wavelength_stop = 1600e-9
+resolution = 0.1
+
+# Grating waveguide compact model (cavity)
+# these are polynomial fit constants from a waveguide width of 500 nm
+n1_g = 4.077182700600432
+n2_g = -0.982556173493906
+n3_g = -0.046366956781710
+
+# Cavity waveguide compact model (cavity)
+# these are polynomial fit constants from a waveguide width of 500 nm
+n1_c = 4.077182700600432
+n2_c = -0.982556173493906
+n3_c = -0.046366956781710
+
+# grating parameters
+kappa = 75000       # coupling strength (/m)
+period = 316e-9     # period of pertrubation
+N_left = 160        # number of periods (left of cavity)
+N_right = 160       # number of periods (right of cavity)
+
+# Cavity Parameters
+alpha = 150/4.34
+L = period/2    # length of cavity
+
+
+#%% analysis
+
+j = cmath.sqrt(-1)
+
+lambda_0 = np.linspace(wavelength_start, wavelength_stop, round((wavelength_stop-wavelength_start)*1e9/resolution))
+neff = (n1_g + n2_g*(lambda_0*1e6) + n3_g*(lambda_0*1e6)**2)
+beta = 2*math.pi*neff/lambda_0
+
+neffc = (n1_c + n2_c*(lambda_0*1e6) + n3_c*(lambda_0*1e6)**2)
+betaC = 2*math.pi*neffc/lambda_0
+
+Lg_L = N_left*period
+Lg_R = N_right*period
+deltaB = 2*math.pi*neff/lambda_0-math.pi/period
+omega = csqrt(kappa**2-deltaB**2)
+
+
+t = []; r = []
+count=0
+for i in range(len(lambda_0)):
+    G_L = [
+        [np.cosh(omega[i]*Lg_L)-j*deltaB[i]/omega[i]*np.sinh(omega[i]*Lg_L),
+         -j*kappa*np.sinh(omega[i]*Lg_L)/omega[i]],
+         [j*kappa*np.sinh(omega[i]*Lg_L)/omega[i],
+          np.cosh(omega[i]*Lg_L)+j*deltaB[i]/omega[i]*np.sinh(omega[i]*Lg_L)]]
+    
+    Y = [[np.exp(-alpha*L/2)*np.exp(-j*betaC[i]*L), 0], [0, np.exp(alpha*L/2)*np.exp(j*betaC[i]*L)]]
+    
+    G_R = [
+        [np.cosh(omega[i]*Lg_R)-j*deltaB[i]/omega[i]*np.sinh(omega[i]*Lg_R),
+         -j*kappa*np.sinh(omega[i]*Lg_R)/omega[i]],
+         [j*kappa*np.sinh(omega[i]*Lg_R)/omega[i],
+          np.cosh(omega[i]*Lg_R)+j*deltaB[i]/omega[i]*np.sinh(omega[i]*Lg_R)]]
+        
+    # matrix multiplication G_L*Y*G_R
+    H1 = np.matmul(G_L,Y)
+    H = np.matmul(H1,G_R)
+    
+    count = -H[1][0]/H[1][1]
+    t.append(H[0][0]-H[1][0]*H[0][1]/H[1][1])
+    r.append(-H[1][0]/H[1][1])
+    
+# to log scale
+T = np.log10(np.absolute(t)**2)
+R = np.log10(np.absolute(r)**2)
+
+#%% plot spectrum
+matplotlib.pyplot.figure(0)
+fig1 = matplotlib.pyplot.plot(lambda_0*1e9,T, label='Transmission', color='blue')
+fig2 = matplotlib.pyplot.plot(lambda_0*1e9,R, label='Reflection', color='red')
+matplotlib.pyplot.legend(loc=0)
+matplotlib.pyplot.ylabel('Response (dB)', color = 'black')
+matplotlib.pyplot.xlabel('Wavelength (nm)', color = 'black')
+matplotlib.pyplot.setp(fig1, 'linewidth', 2.0)
+matplotlib.pyplot.setp(fig2, 'linewidth', 2.0)
+matplotlib.pyplot.xlim(round(min(lambda_0*1e9)),round(max(lambda_0*1e9)))
+matplotlib.pyplot.title("Calculated response of the structure using CMT")
+matplotlib.pyplot.savefig('bragg_CMT'+'.pdf')
+matplotlib.rcParams.update({'font.size': 14, 'font.family' : 'Times New Roman', 'font.weight': 'bold'})
