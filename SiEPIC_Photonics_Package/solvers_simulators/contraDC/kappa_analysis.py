a4b1adf1dfb6557f283865f0de9e4274beefbd5e
mustafacc <mustafa@ece.ubc.ca>
2019-03-26 18:29:29 -0700

    remove kappa_analysis.py
    kappa_analysis is now part of analysis.py

--- SiEPIC_Photonics_Package/solvers_simulators/contraDC/kappa_analysis.py
+++
@@ -1,9 +0,0 @@
-"""
-    kappa_analysis.py
-    Contra-directional coupler Lumerical simulation flow
-    see [] for documentation
-
-    Author: Mustafa Hammood   ; mustafa@siepic.com   ; mustafa@ece.ubc.ca
-    SiEPIC Kits Ltd. 2019     ; University of British Columbia
-
-"""
