ad9299870dcf0009cccff734efe613b215759b9b
mustafacc <mustafa@ece.ubc.ca>
2019-03-29 15:59:10 -0700

    solvers: contraDC: updated EME solver to use PML boundary conidtion

--- SiEPIC_Photonics_Package/solvers_simulators/contraDC/main.py
+++ SiEPIC_Photonics_Package/solvers_simulators/contraDC/main.py
@@ -15,7 +15,7 @@ class contra_DC():
         # physical geometry parameters
         self.w1 = 560e-9
         self.w2 = 440e-9
-        self.dW1 = 24e-9
+        self.dW1 = 48e-9
         self.dW2 = 24e-9
         self.gap = 100e-9
         self.period = 318e-9
@@ -49,7 +49,7 @@ class contra_DC():
 class simulation():
     def __init__(self, *args):
         # make sure range is large enouh to capture all Bragg coupling conditions (both self-Bragg and contra-coupling)
-        self.lambda_start = 1480e-9
+        self.lambda_start = 1500e-9
         self.lambda_end = 1600e-9
         self.resolution = 501
         
@@ -64,9 +64,10 @@ simulation = simulation()
 
 #%% main program
 [waveguides, simulation] = dispersion_analysis.phaseMatch_analysis(device, simulation)
-device = dispersion_analysis.kappa_analysis(device, simulation, waveguides, sim_type = 'EME')
+device = dispersion_analysis.kappa_analysis(device, simulation, waveguides, sim_type = 'EME', close = False)
 device = contraDC_CMT_TMM.contraDC_model(device, simulation, waveguides)
 
 #%% analysis and export parameters
 analysis.plot_all(device, simulation)
-analysis.gen_sparams(device, simulation)
+S = analysis.gen_sparams(device, simulation)
+analysis.performance(S)
